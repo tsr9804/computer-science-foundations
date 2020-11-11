03e2c8f6ea893b473f4036f953fb3ab976ec5b02
Sam Riggs <tsr9804@nyu.edu>
2020-11-11 18:55:17 -0500

    bmimetric commit

---
+++ bmimetric.py
@@ -0,0 +1,7 @@
+def run_bmimetric():
+    weight = float(input("Please  enter weight  in  kilograms: "))
+    height = float(input("Please  enter height  in  meters: "))
+    return weight / (height**2)
+
+if __name__ == "__main__":
+    print(run_bmimetric())
