bc6801b62f977665df2867a73ea8f7dd94272f19
Sam Riggs <tsr9804@nyu.edu>
2020-11-11 19:02:06 -0500

    update

--- bmimetric.py
+++ bmimetric.py
@@ -1,7 +1,3 @@
-def run_bmimetric():
-    weight = float(input("Please  enter weight  in  kilograms: "))
-    height = float(input("Please  enter height  in  meters: "))
-    return f"BMI sss: {weight / (height**2)}"
-
-if __name__ == "__main__":
-    print(run_bmimetric())
+weight = float(input("Please  enter weight  in  kilograms: "))
+height = float(input("Please  enter height  in  meters: "))
+print(f"BMI is: {weight / (height**2)}")
